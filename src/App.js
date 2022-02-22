6d0445d3d0f5ed8e27b9553b19e4f432fd0a5d3c
Rick <rick@ricks-MacBook-Pro.local>
2022-02-22 21:45:47 +0900

    simple react funcs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const Hello = () => {
+    useEffect(() => {
+      console.log("hi");
+      return () => console.log("bye");
+    }, []);
+    return <h1 className={styles.title}>Hello! welcome</h1>;
+  };
+  const [counter, setCounter] = useState(0);
+  const [keyWord, setKeyWord] = useState("");
+  const [showing, setShowing] = useState(false);
+  const onClick2 = () => setShowing((prev) => !prev);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+  const onChange = (event) => setKeyWord(event.target.value);
+  const iroo = () => {
+    console.log(" i run only once ");
+  };
+  useEffect(iroo, []);
+  useEffect(() => {
+    if (keyWord !== "" && keyWord.length > 4) {
+      console.log("SEARCH FOR", keyWord);
+    }
+  }, [keyWord]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
+      <input
+        value={keyWord}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h4>{counter}</h4>
+      <Button text="button text" />
+      <button onClick={onClick}> click </button>
     </div>
   );
 }
